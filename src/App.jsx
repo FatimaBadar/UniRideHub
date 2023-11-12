7a56c586b886ee67c14f1c21a50aa1864322b186
Bilal-01 <mbilalaziz.01@gmail.com>
2023-11-12 18:00:49 +0500

    Add routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  // other routes defined here
+
+]);
 
 function App() {
 
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   )
 }
