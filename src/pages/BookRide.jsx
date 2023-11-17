baecfe81cd42e8b128ffbf5ae308d10183318f70
Alishah-Naushad <alishahnaushad987@gmail.com>
2023-11-17 16:55:36 +0500

    Complete My Ride Page

---
+++ src/pages/BookRide.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+ 
+const BookRide = () => {
+    return(
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
+
+export default BookRide;
