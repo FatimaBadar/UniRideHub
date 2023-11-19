c1808a416187ce9b8ce7c15773949a82d21b1b83
FatimaBadar <fatimabadarq@gmail.com>
2023-11-19 15:31:31 +0500

    added navbar and footer

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,15 @@
 import { useState, React, useContext } from "react";
 import Navbar from "../components/Navbar";
 import ProfileComponent from "../components/ProfileComponent";
+import Footer from "../components/Footer";
 
 export default function Profile() {
 
   return (
     <>
-    {/* <Navbar/> */}
+    <Navbar/>
     <ProfileComponent/>
+    <Footer/>    
     </>
   )
 }
--- src/pages/RequestedRide.jsx
+++ src/pages/RequestedRide.jsx
@@ -1,13 +1,15 @@
 import {React } from "react";
 import Navbar from "../components/Navbar";
 import RequestedRideComponent from "../components/RequestedRideComponent";
+import Footer from "../components/Footer";
 
 export default function RegisteredRide() {
 
   return (
     <>
-    {/* <Navbar/> */}
+    <Navbar/>
     <RequestedRideComponent/>
+    <Footer/>    
     </>
   )
 }
